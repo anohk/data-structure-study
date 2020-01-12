b2dcf1855e8d9c157496571fe16e200b1e252ca4
anohk <anyoung.noh@gmail.com>
2020-01-12 19:09:35 +0900

    CHG: closing brace case 수정

--- stack/brace_check.py
+++ stack/brace_check.py
@@ -20,12 +20,14 @@ braces = {
 
 def check_brace(input_string):
     stack = Stack()
-    open_brace_list = braces.keys()
     for char in input_string:
-        if char in open_brace_list:
+        if char in braces.keys():
             stack.push(char)
-        elif stack.top is not None and char == braces[stack.top.data]:
-            stack.pop()
+        elif char in braces.values():
+            if stack.count > 0 and char == braces[stack.top.data]:
+                stack.pop()
+            elif stack.count == 0 or char != braces[stack.top.data]:
+                return 'no'
 
     if stack.count == 0:
         return 'yes'
@@ -51,9 +53,10 @@ class BraceCheckTestCase(unittest.TestCase):
         assert check_brace('[ first in ] ( first out ).') == 'yes'
         assert check_brace('([ (([( [ ] ) ( ) (( ))] )) ]).') == 'yes'
 
-        assert check_brace('Half Moon tonight (At least it is better than no Moon at all].') == 'no'
+        assert check_brace('Half Moon tonight (At least it is better than no Moon at all]).') == 'no'
         assert check_brace('A rope may form )( a trail in a maze.') == 'no'
         assert check_brace('Help( I[m being held prisoner in a fortune cookie factory)].') == 'no'
+        assert check_brace('{](( )} (( ))] )) ]).') == 'no'
 
 
 if __name__ == '__main__':
